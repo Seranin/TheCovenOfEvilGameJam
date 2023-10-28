c674593ce5f4e34b6b5a5a215f99388ead38fa97
JorritR <jorrit.ruelens@student.ucll.be>
2023-10-28 09:39:36 +0200

    Update playerMovement.cs

--- Elemental/Assets/JorritFuckoff/playerMovement.cs
+++ Elemental/Assets/JorritFuckoff/playerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Callbacks;
 using UnityEngine;
 
 public class playerMovement : MonoBehaviour
@@ -9,7 +10,8 @@ public class playerMovement : MonoBehaviour
     public float verticalMovement;
     public float speed = 8;
     public Vector2 movement;
-
+    public Vector2 mousePosition;
+    public Vector2 aimDirection;
 
     // Start is called before the first frame update
     void Start()
@@ -22,12 +24,17 @@ public class playerMovement : MonoBehaviour
     {
         verticalMovement = Input.GetAxisRaw("Vertical");
         horizontalMovement =  Input.GetAxisRaw("Horizontal");
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
 
     void FixedUpdate()
     {
+        //movement
         movement = new Vector2(horizontalMovement, verticalMovement).normalized;
         rg.velocity = new Vector2(movement.x*speed,movement.y*speed);
-        
+        //aiming
+        aimDirection= mousePosition-rg.position;
+        float aimAngle =Mathf.Atan2(aimDirection.y,aimDirection.x)*Mathf.Rad2Deg -90f;
+        rg.rotation = aimAngle;
     }
 }
