f77f6fa11c22ebf68366338d057ba028194773bb
Seranin <66880421+Seranin@users.noreply.github.com>
2023-10-29 16:08:59 +0100

    Milk Push

--- Elemental/Assets/CatAnimLogic.cs
+++ Elemental/Assets/CatAnimLogic.cs
@@ -6,6 +6,9 @@ public class CatAnimLogic : MonoBehaviour
 {
     // Start is called before the first frame update
     Animator anim;
+    public playerMovement playerMovement;
+    private Vector2 dashvec;
+
     void Start()
     {
         anim = GetComponent<Animator>();
@@ -17,6 +20,30 @@ public class CatAnimLogic : MonoBehaviour
         if(Input.GetMouseButtonDown(0))
         {
             anim.SetTrigger("LeftClick");
+        }
+    }
+    void LateUpdate()
+    {
+        if(Input.GetButton("Jump")){
+            dashvec = playerMovement.movement * playerMovement.dashspeed;
+            if(dashvec.y > dashvec.x)
+            {
+                if(dashvec.y >= 0){
+                    anim.SetTrigger("DashUp");
+                }
+                else{
+                    anim.SetTrigger("DashDown");
+                }
+            }
+            else{
+                if(dashvec.x >= 0)
+                {
+                    anim.SetTrigger("DashRight");
+                }
+                else{
+                    anim.SetTrigger("Dashleft");
+                }
+            }
         }
     }
 }
