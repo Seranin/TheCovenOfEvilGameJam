6054a262c09141b4cc9056e0b660b80a55556606
Seranin <66880421+Seranin@users.noreply.github.com>
2023-10-29 10:11:37 +0100

    player prefab

--- Elemental/Assets/CameraMovement.cs
+++ Elemental/Assets/CameraMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
     public Transform playerPos;
+    public float lerpStr = 5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +16,7 @@ public class CameraMovement : MonoBehaviour
     void FixedUpdate()
     {
         //transform.position = playerPos.position + new Vector3(0, 0, -10f);
-        Vector3 target = playerPos.position + new Vector3(0, 0, -10f);
-        transform.position =  Vector3.Lerp(transform.position, target, Time.deltaTime * 5f);
+        Vector3 target = playerPos.position + new Vector3(0, 0, -15f);
+        transform.position =  Vector3.Lerp(transform.position, target, Time.deltaTime * lerpStr);
     }
 }
