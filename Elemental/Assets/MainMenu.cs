616753bf47d4462f8a44bc07f1b477b0e974f84c
Eric Andoni <108674878+AndoniEric@users.noreply.github.com>
2023-10-29 14:46:53 +0100

    working main menu
    i cry

---
+++ Elemental/Assets/MainMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        //Go to next scene; which can be found in File -> Build settings
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
